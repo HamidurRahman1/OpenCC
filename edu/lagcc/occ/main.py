a606266f77ad91acc2c33f6302f080a598d0c8e2
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-12 23:08:59 -0500

    class search returing results

--- edu/lagcc/occ/main.py
+++ edu/lagcc/occ/main.py
@@ -1,16 +1,22 @@
 
-from edu.lagcc.occ.util.Utils import URL, CLG_TRM_FORM, CLG_TRM_NAME_KEY, CLG_TRM_VAL_KEY
+from edu.lagcc.occ.util.Utils import *
 from datetime import datetime
 import requests as req
 import time
 
 dt = datetime.now()
-term = "2021 Spring Term"
-term_val = "1212"
 
 session = req.Session()
-CLG_TRM_FORM[CLG_TRM_NAME_KEY] = term
-CLG_TRM_FORM[CLG_TRM_VAL_KEY] = term_val
+
+CLG_TRM_FORM[CLG_TRM_NAME_KEY] = "2021 Spring Term"
+CLG_TRM_FORM[CLG_TRM_VAL_KEY] = "1212"
 res = session.post(URL, data=CLG_TRM_FORM)
 time.sleep(1)
 print(res.status_code, datetime.now()-dt)
+
+CLASS_DETAILS_FORM[SUBJECT_NAME_KEY] = "VETE"
+CLASS_DETAILS_FORM[SESSION_KEY] = "1"
+res2 = session.post(URL, data=CLASS_DETAILS_FORM)
+print(res2.status_code, datetime.now()-dt)
+print(res2.text)
+
--- edu/lagcc/occ/util/Utils.py
+++ edu/lagcc/occ/util/Utils.py
@@ -1,9 +1,32 @@
 
 URL = "https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController"
-NEXT = "Next"
 
 CLG_TRM_NAME_KEY = "selectedTermName"
 CLG_TRM_VAL_KEY = "term_value"
-CLG_TRM_FORM = {"selectedInstName": "LaGuardia CC", "inst_selection": "LAG01",
-                CLG_TRM_NAME_KEY: '', CLG_TRM_VAL_KEY: '', "next_btn": NEXT}
+CLG_TRM_FORM = {"selectedInstName":     "LaGuardia CC",
+                "inst_selection":       "LAG01",
+                CLG_TRM_NAME_KEY:       "",
+                CLG_TRM_VAL_KEY:        "",
+                "next_btn":             "Next"}
+
+
+SESSION_KEY = "class_session"
+SUBJECT_NAME_KEY = "subject_name"
+CLASS_DETAILS_FORM = {SUBJECT_NAME_KEY:             "",
+                      "selectedSessionName":        "",
+                      SESSION_KEY:                  "",
+                      "meetingStart":               "LT",
+                      "selectedMeetingStartName":   "less than",
+                      "meetingStartText":           "",
+                      "AndMeetingStartText":        "",
+                      "meetingEnd":                 "LE",
+                      "selectedMeetingEndName":     "less than or equal to",
+                      "meetingEndText":             "",
+                      "AndMeetingEndText":          "",
+                      "daysOfWeek":                 "I",
+                      "selectedDaysOfWeekName":     "include only these days",
+                      "instructor":                 "B",
+                      "selectedInstructorName":     "begins with",
+                      "instructorName":             "",
+                      "search_btn_search":          "Search"}
 
