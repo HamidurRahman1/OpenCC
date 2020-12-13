ad6452fce40b73cee37e2cc4be549904202395d6
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-12 21:28:05 -0500

    util added

--- edu/lagcc/opencc/main.py
+++ edu/lagcc/opencc/main.py
@@ -1,15 +1,19 @@
+
+from edu.lagcc.opencc.util.Utils import COLLEGE, CLG_VALUE, URL, NEXT
+
+from datetime import datetime
 import requests as req
 import time
 
-COLLEGE = "LaGuardia CC"
-CLG_VALUE = "LAG01"
+dt = datetime.now()
 term = "2021 Spring Term"
-URL = "https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController"
+term_val = "1212"
 
 clg_selector_form_data = {"selectedInstName": COLLEGE, "inst_selection": CLG_VALUE, "selectedTermName": term,
-                        "term_value":"1212", "next_btn":"Next"}
+                        "term_value": term_val, "next_btn": NEXT}
 
 session = req.Session()
 res = session.post(URL, data=clg_selector_form_data)
 time.sleep(1)
 print(res.status_code, res.text)
+print(datetime.now()-dt)
---
+++ edu/lagcc/opencc/util/Utils.py
@@ -0,0 +1,5 @@
+
+COLLEGE = "LaGuardia CC"
+CLG_VALUE = "LAG01"
+URL = "https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController"
+NEXT = "Next"
