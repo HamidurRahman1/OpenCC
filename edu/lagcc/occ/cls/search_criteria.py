53de31cd858b4ed7fea0ec0075408568265e5f6a
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-13 18:46:25 -0500

    successfully located class num and status

--- edu/lagcc/occ/cls/search_criteria.py
+++ edu/lagcc/occ/cls/search_criteria.py
@@ -1,18 +1,36 @@
 
 from edu.lagcc.occ.util.Utils import *
+from bs4 import BeautifulSoup
 import requests
+import re
 
 
 class SearchCriteriaPage:
 
-    def __init__(self):
+    def __init__(self, term_name, subject_code, class_num_5_digit):
         self.session = requests.Session()
+        self.term_name = term_name
+        self.subject_code = subject_code
+        self.class_num_5_digit = class_num_5_digit
+        self.status = False
 
-    def search_criteria_response(self, term_name, term_value, subject_code, session_code):
-        CLG_TRM_FORM[CLG_TRM_NAME_KEY] = term_name
-        CLG_TRM_FORM[CLG_TRM_VAL_KEY] = term_value
+    def check_session_one(self):
+        CLG_TRM_FORM[CLG_TRM_NAME_KEY] = self.term_name
+        CLG_TRM_FORM[CLG_TRM_VAL_KEY] = "1212"
         self.session.post(URL, data=CLG_TRM_FORM)
-        CLASS_DETAILS_FORM[SUBJECT_NAME_KEY] = subject_code
-        CLASS_DETAILS_FORM[SESSION_KEY] = session_code
-        response = self.session.post(URL, data=CLASS_DETAILS_FORM)
-        return response
+        CLASS_DETAILS_FORM[SUBJECT_NAME_KEY] = self.subject_code
+        CLASS_DETAILS_FORM[SESSION_KEY] = "1"
+        soup = BeautifulSoup(self.session.post(URL, data=CLASS_DETAILS_FORM).content, 'html.parser')
+        results = soup.find_all("td", {"class": "cunylite_LEVEL3GRIDROW"})
+        i = 0
+        for elem in results:
+            val = elem.text.strip()
+            if re.match("^\\d+$", val) and self.class_num_5_digit == val:
+                print("found", results[i+7])
+                print(elem.find_next("img")["title"])
+                # notify if open
+                if elem.find_next("img")["title"] == "Open":
+                    self.status = True
+                    break
+            i = i+1
+        return self
--- edu/lagcc/occ/main.py
+++ edu/lagcc/occ/main.py
@@ -1,22 +1,19 @@
 
 from edu.lagcc.occ.cls.search_criteria import SearchCriteriaPage
 from datetime import datetime
-from bs4 import BeautifulSoup
 
 dt = datetime.now()
 
 if __name__ == "__main__":
 
     term = "2021 Spring Term"
-    term_value = "1212"
     subject_code = "VETE"
-    session_code = "1"
+    class_num_5_digit = "39335"
 
     print(datetime.now()-dt)
-    results = SearchCriteriaPage().search_criteria_response(term, term_value, subject_code, session_code)
-    print(datetime.now()-dt)
-    soup = BeautifulSoup(results.text, 'html.parser')
-    class_names = soup.find_all("span", {"class": "cunylite_LABEL"})
-    for i in class_names:
-        print(i.text.strip())
+    obj = SearchCriteriaPage(term, subject_code, class_num_5_digit).check_session_one()
+    if obj.status:
+        print("open")
+    else:
+        print(obj.status)
     print(datetime.now()-dt)
