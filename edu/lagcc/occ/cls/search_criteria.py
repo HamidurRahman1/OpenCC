e90e569c37f402164e9df1efb85d26a27fe54b21
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-13 21:20:36 -0500

    search for 2nd session added

--- edu/lagcc/occ/cls/search_criteria.py
+++ edu/lagcc/occ/cls/search_criteria.py
@@ -5,29 +5,40 @@ import requests
 import re
 
 
-class SearchCriteriaPage:
+class SearchCriteria:
 
     def __init__(self, term_name, subject_code, class_num_5_digit):
         self.session = requests.Session()
         self.term_name = term_name
+        self.__term_value = "1212"                  # determine from prop file using term_name
         self.subject_code = subject_code
         self.class_num_5_digit = class_num_5_digit
         self.status = False
+        self.found = False
+        self.clg_trm_dict = CLG_TRM_FORM.copy()
+        self.cls_details_dict = CLASS_DETAILS_FORM.copy()
 
     def check_session_one(self):
-        CLG_TRM_FORM[CLG_TRM_NAME_KEY] = self.term_name
-        CLG_TRM_FORM[CLG_TRM_VAL_KEY] = "1212"
-        self.session.post(URL, data=CLG_TRM_FORM)
-        CLASS_DETAILS_FORM[SUBJECT_NAME_KEY] = self.subject_code
-        CLASS_DETAILS_FORM[SESSION_KEY] = "1"
-        soup = BeautifulSoup(self.session.post(URL, data=CLASS_DETAILS_FORM).content, 'html.parser')
+        return self.__do_extraction("1")
+
+    def check_session_two(self):
+        return self.__do_extraction("2")
+
+    def __do_extraction(self, session_code):
+        self.clg_trm_dict[CLG_TRM_NAME_KEY] = self.term_name
+        self.clg_trm_dict[CLG_TRM_VAL_KEY] = self.__term_value
+        self.session.post(URL, data=self.clg_trm_dict)
+        self.cls_details_dict[SUBJECT_NAME_KEY] = self.subject_code
+        self.cls_details_dict[SESSION_KEY] = session_code
+        soup = BeautifulSoup(self.session.post(URL, data=self.cls_details_dict).content, 'html.parser')
         results = soup.find_all("td", {"class": "cunylite_LEVEL3GRIDROW"})
         i = 0
         for elem in results:
             val = elem.text.strip()
             if re.match("^\\d+$", val) and self.class_num_5_digit == val:
-                print("found", results[i+7])
-                print(elem.find_next("img")["title"])
+                self.found = True
+                print("==> found", results[i+7])
+                print("==> ", elem.find_next("img")["title"])
                 # notify if open
                 if elem.find_next("img")["title"] == "Open":
                     self.status = True
--- edu/lagcc/occ/main.py
+++ edu/lagcc/occ/main.py
@@ -1,5 +1,5 @@
 
-from edu.lagcc.occ.cls.search_criteria import SearchCriteriaPage
+from edu.lagcc.occ.cls.search_criteria import SearchCriteria
 from datetime import datetime
 
 dt = datetime.now()
@@ -8,12 +8,10 @@ if __name__ == "__main__":
 
     term = "2021 Spring Term"
     subject_code = "VETE"
-    class_num_5_digit = "39335"
+    class_num_5_digit = "39345"
 
     print(datetime.now()-dt)
-    obj = SearchCriteriaPage(term, subject_code, class_num_5_digit).check_session_one()
-    if obj.status:
-        print("open")
-    else:
-        print(obj.status)
+    obj = SearchCriteria(term, subject_code, class_num_5_digit).check_session_one()
+    if not obj.found:
+        obj.check_session_two()
     print(datetime.now()-dt)
