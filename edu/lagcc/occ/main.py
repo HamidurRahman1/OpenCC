cb894437b3a9ad70cd6cbbe13c8b0304656b50ac
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-14 19:51:06 -0500

    prop files updated

--- edu/lagcc/occ/main.py
+++ edu/lagcc/occ/main.py
@@ -1,5 +1,6 @@
 
 from edu.lagcc.occ.cls.search_criteria import SearchCriteria
+from edu.lagcc.occ.util.Utils import *
 from datetime import datetime
 
 dt = datetime.now()
@@ -9,6 +10,8 @@ if __name__ == "__main__":
     term = "2021 Spring"
     subject_code = "VETE"
     class_num_5_digit = "55576"
+
+    print(TERMS_VALUES_DICT)
 
     print(datetime.now()-dt)
     obj = SearchCriteria(term, subject_code, class_num_5_digit).check_session_one()
--- edu/lagcc/occ/util/Utils.py
+++ edu/lagcc/occ/util/Utils.py
@@ -3,7 +3,7 @@ import os
 import logging
 
 APP_NAME = "OpenCC"
-TERMS_VALUES_FILE_PATH = os.path.join(os.path.dirname(__file__), '../props/terms_values.txt')
+TERMS_VALUES_FILE_PATH = os.path.join(os.path.dirname(__file__), '../props/terms_values.properties')
 URL = "https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController"
 LOG_FILE_PATH = os.path.join(os.path.dirname(__file__), '../logs/logs.log')
 LOG_FORMAT = "$ "+APP_NAME+" ---> %(levelname)s :: %(message)s"
@@ -15,7 +15,7 @@ def load_terms_values():
     f_obj = open(TERMS_VALUES_FILE_PATH)
     for line in f_obj.readlines():
         term_val = line.split("=")
-        terms_dict[term_val[0]] = term_val[1].strip()
+        terms_dict[term_val[1].strip()] = term_val[0].strip()
     f_obj.close()
     return terms_dict
 
