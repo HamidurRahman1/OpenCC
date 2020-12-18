d5d1ff067f2c9b5971ae9a2eef737582a33461d0
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-18 12:42:18 -0500

    rm log file path

--- edu/lagcc/occ/config/starter.py
+++ edu/lagcc/occ/config/starter.py
@@ -1,17 +1,10 @@
 
 import os
-import logging
 
 APP_NAME = "OpenCC"
 
-ROOT_DIR = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir + os.sep + os.pardir)
-
 TERMS_VALUES_FILE_PATH = os.path.join(os.path.dirname(__file__), "../props/terms_values.properties")
 SUB_CODES_FILE_PATH = os.path.join(os.path.dirname(__file__), "../props/sub_codes.properties")
-LOG_FILE_PATH = os.path.join(ROOT_DIR, "edu/lagcc/occ/log/logs.log")
-
-LOG_FORMAT = "$ "+APP_NAME+" ---> %(levelname)s :: %(message)s"
-logging.basicConfig(filename=LOG_FILE_PATH, filemode='a', level=logging.INFO, format=LOG_FORMAT)
 
 
 def load_terms_values():
