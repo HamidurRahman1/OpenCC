adcafecf905d99dfc0317ea17363024e2fa67be3
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-18 10:26:52 -0500

    printing env values

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from edu.lagcc.occ.services.term_service import TermService
 from edu.lagcc.occ.singletons.flask_instance import SingletonFlaskInstance
 from edu.lagcc.occ.singletons.mysql_instance import SingletonMySQLInstance
-
+from os import environ
 
 if __name__ == "__main__":
 
@@ -16,6 +16,8 @@ if __name__ == "__main__":
         # for t in ts.get_all_terms():
         #     print(t)
         # print(ts.get_term_by_name("2025 Spring"))
+        for key in environ:
+            print("=> ", environ.get(key))
         return "my home page 1"
 
     app.run()
