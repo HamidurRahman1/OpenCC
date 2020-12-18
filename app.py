2c9bc0bd569bb34f96de750277f05ab26489ebd5
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-18 11:19:14 -0500

    var visibility issue for heroku

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from edu.lagcc.occ.singletons.flask_instance import SingletonFlaskInstance
 from edu.lagcc.occ.singletons.mysql_instance import SingletonMySQLInstance
 from os import environ
 
+app = SingletonFlaskInstance.get_instance()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    s = "===> \n"
+    for key in environ:
+        s = s + key + "=> " + environ.get(key)
+    return s+"\n <==="
+
+
 if __name__ == "__main__":
-
-    app = SingletonFlaskInstance.get_instance()
-    # mysql = SingletonMySQLInstance.get_instance()
-
-
-    @app.route('/', methods=['GET'])
-    def index():
-        s = "===> \n"
-        for key in environ:
-            s = s + key + "=> " + environ.get(key)
-        return s+"\n <==="
 
     app.run()
 
