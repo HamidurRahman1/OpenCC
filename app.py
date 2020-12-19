2c50c4c15a4068c9dc6aba22edd70fcff2c82423
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-19 14:17:19 -0500

    user repo added

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
-from edu.lagcc.occ.repositories.subject_repo import SubjectRepository
-from edu.lagcc.occ.repositories.term_repo import TermRepository
+from edu.lagcc.occ.repositories.user_repo import UserRepository
 from edu.lagcc.occ.singletons.flask_instance import SingletonFlaskInstance
 from edu.lagcc.occ.singletons.mysql_instance import SingletonMySQLInstance
 from os import environ
@@ -11,9 +10,7 @@ mysql = SingletonMySQLInstance.get_instance()
 
 @app.route('/', methods=['GET'])
 def index():
-    for i in SubjectRepository(mysql.connection).get_all_subjects():
-        print(i)
-    return "home"
+    return UserRepository(mysql.connection).get_user_by_id(4).__str__()
 
     # s = "===> \n"
     # for key in environ:
---
+++ edu/lagcc/occ/repositories/user_repo.py
@@ -0,0 +1,17 @@
+
+from edu.lagcc.occ.models.user import User
+
+
+class UserRepository:
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_user_by_id(self, user_id):
+        query = """select * from users where user_id = %s """ % user_id
+        cur = self.connection.cursor()
+        cur.execute(query)
+        user = cur.fetchone()
+        cur.close()
+        return User(user_id=user[0], phone_number=user[1])
+
