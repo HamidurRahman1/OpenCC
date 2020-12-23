19044f0a7e8d70b46ebf799af1894a16d324abc8
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-23 17:38:14 -0500

    account creation message added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask_mysqldb import MySQL
 from edu.lagcc.occ.config.starter import APP_NAME
+from edu.lagcc.occ.notifier.sms_sender import SMSSender
 from edu.lagcc.occ.repositories.term_repo import TermRepository
 from edu.lagcc.occ.task.search_invoker import init
 
--- edu/lagcc/occ/notifier/sms_sender.py
+++ edu/lagcc/occ/notifier/sms_sender.py
@@ -9,11 +9,16 @@ class SMSSender:
     __auth_token = environ["TWILIO_AUTH_TOKEN"]
     __from = environ["TWILIO_NUMBER"]
 
-    def __init__(self, phone, subject, class_num_5_digit, term):
+    def __init__(self, phone, subject, class_num_5_digit, term, is_first=False):
         self.__client = Client(SMSSender.__account_sid, SMSSender.__auth_token)
         self.__to = "+1"+str(phone)
         self.__from_ = SMSSender.__from
-        self.__body = "Dear OpenCC user, your requested class {} - {} for {} is now OPEN. Good Luck!".format(subject, class_num_5_digit, term)
+        if is_first:
+            self.__body = "Dear user, thank you for choosing to use OpenCC to get notified as soon as " \
+                          "your requested classes become open. I have got you with your classes, 🤝. Cheers!"
+        else:
+            self.__body = "Dear OpenCC user, your requested class {} - {} for {} is now OPEN ✅. Good Luck!"\
+                            .format(subject, class_num_5_digit, term)
 
     def send(self):
         self.__client.api.account.messages.create(to=self.__to, from_=self.__from, body=self.__body)
