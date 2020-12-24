1ff07e0dc4bfa03f17506051578e5c61e2df728b
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-24 12:53:31 -0500

    sms send successfully

--- app.py
+++ app.py
@@ -45,15 +45,15 @@ mysql = MySQLInstance.get_instance()
 
 @app.route("/", methods=["GET"])
 def index():
-        d = RequestRepository(mysql.connection).get_requests_to_search_and_notify()
-        for k in d.keys():
-            print(k, "==>")
-            for j in d.get(k):
-                print(j)
-        return render_template("index.html")
+    d = RequestRepository(mysql.connection).get_requests_to_search_and_notify()
+    for k in d.keys():
+        print(k, "==>")
+        for j in d.get(k):
+            print(j)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    # class_search_scheduler()
+    class_search_scheduler()
     app.run(use_reloader=False)
 
--- edu/lagcc/opencc/task/search_invoker.py
+++ edu/lagcc/opencc/task/search_invoker.py
@@ -4,6 +4,7 @@ import threading
 import time
 import MySQLdb
 from os import environ
+from edu.lagcc.opencc.notifier.sms_sender import SMSSender
 from edu.lagcc.opencc.repositories.request_repo import RequestRepository
 from edu.lagcc.opencc.searcher.class_searcher import OpenClassSearcher
 
@@ -13,19 +14,18 @@ def _search(class_num, requests_set):
     obj = OpenClassSearcher(req_obj.term.term_name, req_obj.subject.subject_code, class_num).check_session_one()
     if obj.found:
         if obj.status:
-            # send text to all users in requests_set
-            print("notify", len(requests_set), "users")
+            SMSSender(req_obj.user.phone_number, req_obj.subject.subject_name, class_num, req_obj.term.term_name).send()
+            print(len(requests_set), "users notified")
         else:
-            print("class still closed in s1")
+            print("class still closed in session 1")
     else:
         obj = obj.check_session_two()
-        print("check 2")
         if obj.found:
             if obj.status:
-                # send text to all users in requests_set
-                print("notify", len(requests_set), "users")
+                SMSSender(req_obj.user.phone_number, req_obj.subject.subject_name, class_num, req_obj.term.term_name).send()
+                print(len(requests_set), "users notified")
             else:
-                print("class still closed in s2")
+                print("class still closed in session 2")
 
 
 def _process(class_num_to_req_dict):
@@ -41,17 +41,17 @@ def _process(class_num_to_req_dict):
 
 def _invoke_class_searcher():
     start = time.time()
-    expected_end = 60
+    expected_end = 120
 
     connection = MySQLdb.connect(host=environ.get("MYSQL_HOST"), user=environ.get("MYSQL_USER"),
                                  passwd=environ.get("MYSQL_PASSWORD"), db=environ.get("MYSQL_DB"))
 
     class_num_to_requests = RequestRepository(connection).get_requests_to_search_and_notify()
     connection.close()
-    for k in class_num_to_requests.keys():
-        print(k, "==>")
-        for j in class_num_to_requests.get(k):
-            print(j)
+    # for k in class_num_to_requests.keys():
+    #     print(k, "==>")
+    #     for j in class_num_to_requests.get(k):
+    #         print(j)
     ss = time.time()
     _process(class_num_to_requests)
     print(time.time()-ss, "pro")
