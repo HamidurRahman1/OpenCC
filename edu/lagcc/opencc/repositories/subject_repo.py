ca9d9e5ea8b3fe7b803af090dab3439e521d26a2
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-26 14:23:15 -0500

    subject repo exception handled

--- edu/lagcc/opencc/repositories/subject_repo.py
+++ edu/lagcc/opencc/repositories/subject_repo.py
@@ -9,12 +9,16 @@ class SubjectRepository:
 
     def get_all_subjects(self):
         query = "select * from subjects"
-        cur = self.connection.cursor()
-        subjects_list = set()
-        cur.execute(query)
-        for subject in cur.fetchall():
-            subjects_list.add(Subject(subject_id=subject[0], subject_code=subject[1], subject_name=subject[2]))
-        cur.close()
-        return subjects_list
-
-
+        try:
+            cur = self.connection.cursor()
+            subjects_set = set()
+            cur.execute(query)
+            for subject in cur.fetchall():
+                subjects_set.add(Subject(subject_id=subject[0], subject_code=subject[1], subject_name=subject[2]))
+            cur.close()
+            return subjects_set
+        except Exception as ex:
+            template = "Exception of type '{0}' with arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+            pass    # raise DevNotify Exception
