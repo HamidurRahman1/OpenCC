711f6cf0adc62beef11b5a870a72d1db352bde37
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-26 18:29:38 -0500

    duplicate phone number issue resolved

--- edu/lagcc/opencc/repositories/user_repo.py
+++ edu/lagcc/opencc/repositories/user_repo.py
@@ -1,14 +1,21 @@
 
-from MySQLdb._exceptions import IntegrityError
 from edu.lagcc.opencc.models.user import User
+from MySQLdb._exceptions import IntegrityError
+from edu.lagcc.opencc.exceptions.exceptions import NotFoundException
+from edu.lagcc.opencc.exceptions.exceptions import UserExistsException
+from edu.lagcc.opencc.exceptions.exceptions import NotifyDeveloperException
 
 
 class UserRepository:
+
+    """ This repository replicates `users` table in database. The constructor takes a database connection. """
 
     def __init__(self, connection):
         self.connection = connection
 
     def get_user_by_id(self, user_id):
+        """ Returns a User object if an associated user with user_id exists. """
+
         query = """select * from users where user_id = %s """
         try:
             cur = self.connection.cursor()
@@ -16,16 +23,14 @@ class UserRepository:
             user = cur.fetchone()
             cur.close()
             if user is None:
-                print("user does not exists", user_id)
-                return False
+                raise NotFoundException("No user with user_id={} exists".format(user_id))
             return User(user_id=user[0], phone_number=user[1])
         except Exception as ex:
-            template = "Exception of type '{0}' with arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-            pass  # raise DevNotify Exception
+            raise NotifyDeveloperException(type(ex).__name__, ex.args)
 
     def get_user_by_phone_num(self, phone_number):
+        """ Returns a User object if an associated user with phone_num exists. """
+
         query = """select * from users where phone_num = %s """
         try:
             cur = self.connection.cursor()
@@ -33,16 +38,14 @@ class UserRepository:
             user = cur.fetchone()
             cur.close()
             if user is None:
-                print("user does not exists", phone_number)
-                return False
+                raise NotFoundException("No user with phone_number={} exists".format(phone_number))
             return User(user_id=user[0], phone_number=user[1])
         except Exception as ex:
-            template = "Exception of type '{0}' with arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-            pass  # raise DevNotify Exception
+            raise NotifyDeveloperException(type(ex).__name__, ex.args)
 
     def save_user(self, phone_number):
+        """ Creates a user record in the database using the this phone number. """
+
         query = """insert into users (phone_num) value (%s)"""
         try:
             cur = self.connection.cursor()
@@ -52,12 +55,7 @@ class UserRepository:
                 cur.close()
                 return True
         except IntegrityError as ex:
-            template = "Exception of type '{0}' with arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-            pass  # raise DevNotify Exception
+            if "for key 'phone_num'" in ex.args[1]:
+                raise UserExistsException(phone_number=phone_number)
         except Exception as ex:
-            template = "Exception of type '{0}' with arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print(message)
-            pass  # raise DevNotify Exception
+            raise NotifyDeveloperException(type(ex).__name__, ex.args)
