17600ab3d0e8cb0aed71df7c22cb08fd814ea44e
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-25 20:16:05 -0500

    property name changed

--- edu/lagcc/opencc/notifier/sms_sender.py
+++ edu/lagcc/opencc/notifier/sms_sender.py
@@ -5,20 +5,20 @@ from twilio.rest import Client
 
 class SMSSender:
 
-    __account_sid = environ["TWILIO_ACCOUNT_SID"]
-    __auth_token = environ["TWILIO_AUTH_TOKEN"]
-    __from = environ["TWILIO_NUMBER"]
+    __account_sid = environ.get("TWILIO_ACCOUNT_SID")
+    __auth_token = environ.get("TWILIO_AUTH_TOKEN")
+    __from = environ.get("TWILIO_NUMBER")
 
-    def __init__(self, phone, subject, class_num_5_digit, term, is_first=False):
+    def __init__(self, phone_number, subject_name, class_num_5_digit, term_name, is_first=False):
         self.__client = Client(SMSSender.__account_sid, SMSSender.__auth_token)
-        self.__to = "+1"+str(phone)
+        self.__to = "+1"+str(phone_number)
         self.__from_ = SMSSender.__from
         if is_first:
             self.__body = "Dear user, thank you for choosing to use OpenCC to get notified as soon as " \
                           "your requested classes become open. I have got you with your classes, 🤝. Cheers!"
         else:
             self.__body = "Dear OpenCC user, your requested class {} - {} for {} is now OPEN ✅. Good Luck!"\
-                            .format(subject, class_num_5_digit, term)
+                            .format(subject_name, class_num_5_digit, term_name)
 
     def send(self):
         self.__client.api.account.messages.create(to=self.__to, from_=self.__from, body=self.__body)
