93cb32c419e6279e88ed779abcf77e47356709a2
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-26 17:30:17 -0500

    DuplicateRequest added

--- edu/lagcc/opencc/exceptions/exceptions.py
+++ edu/lagcc/opencc/exceptions/exceptions.py
@@ -2,11 +2,23 @@
 class DuplicatePhoneNumber(Exception):
     """Exception raised when account with the given phone number exists.
     Attributes:
-        phone_number -- indicating the existing phone number
-        message -- explanation of the error
+        phone_number --> indicating the existing phone number
     """
 
-    def __init__(self, phone_number, message):
-        self.phone_number = phone_number
-        self.message = message
+    def __init__(self, phone_number):
+        self.message = "A user associated with Phone Number: {} already exists.".format(phone_number)
+        super().__init__(self.message)
+
+
+class DuplicateRequest(Exception):
+    """Exception raised user attempts to make a request for the same class that exists.
+    Attributes:
+        phone_number --> indicating the existing phone number
+        subject_name --> indicates subject
+        class_num_5_digit --> unique 5 digit to identify the class
+    """
+
+    def __init__(self, phone_number, subject_name, class_num_5_digit):
+        self.message = "A request with Phone number: {}, Subject: {}, and Class Number: {} already exists."\
+            .format(phone_number, subject_name, class_num_5_digit)
         super().__init__(self.message)
