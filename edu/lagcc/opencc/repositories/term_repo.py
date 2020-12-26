26d698b10cb5fca7ba6a9a4153d2ee4253b948ce
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-26 14:26:10 -0500

    exception in term repo

--- edu/lagcc/opencc/repositories/term_repo.py
+++ edu/lagcc/opencc/repositories/term_repo.py
@@ -9,18 +9,30 @@ class TermRepository:
 
     def get_all_terms(self):
         query = "select * from terms"
-        cur = self.connection.cursor()
-        terms_list = set()
-        cur.execute(query)
-        for term in cur.fetchall():
-            terms_list.add(Term(term_id=term[0], term_name=term[1], term_value=term[2]))
-        cur.close()
-        return terms_list
+        try:
+            cur = self.connection.cursor()
+            terms_set = set()
+            cur.execute(query)
+            for term in cur.fetchall():
+                terms_set.add(Term(term_id=term[0], term_name=term[1], term_value=term[2]))
+            cur.close()
+            return terms_set
+        except Exception as ex:
+            template = "Exception of type '{0}' with arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+            pass  # raise DevNotify Exception
 
     def get_term_by_name(self, term_name):
         query = """select * from terms where term_name = %s """
-        cur = self.connection.cursor()
-        cur.execute(query, (term_name,))
-        term = cur.fetchone()
-        cur.close()
-        return Term(term_id=term[0], term_name=term[1], term_value=term[2])
+        try:
+            cur = self.connection.cursor()
+            cur.execute(query, (term_name,))
+            term = cur.fetchone()
+            cur.close()
+            return Term(term_id=term[0], term_name=term[1], term_value=term[2])
+        except Exception as ex:
+            template = "Exception of type '{0}' with arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+            pass  # raise DevNotify Exception
