ff54947ce045a950dee42c7f00352dfc58e6a891
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-30 18:54:09 -0500

    unsubscribe request

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
+from os import environ
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask_mysqldb import MySQL
 from edu.lagcc.opencc.exceptions.exceptions import DuplicateRequestException
+from edu.lagcc.opencc.exceptions.exceptions import NotifyDeveloperException
 from edu.lagcc.opencc.notifier.sms_sender import SMSSender
 from edu.lagcc.opencc.repositories.request_repo import RequestRepository
 from edu.lagcc.opencc.utils.util import APP_NAME
@@ -52,8 +54,10 @@ def __add_request__():
     except DuplicateRequestException as dex:
         return "Dear {} user, {} You may add a different request for a different class if necessary."\
                 .format(APP_NAME, str(dex).lower())
-    except Exception as ex:
-        SMSSender(dev=True, dev_msg=ex.args).send()
+    except NotifyDeveloperException as nex:
+        SMSSender(dev=True, dev_msg=nex).send()
+    except:
+        pass
 
 
 @_app.route("/", methods=["GET", "POST"])
@@ -64,6 +68,27 @@ def index():
         status = __add_request__()
         return render_template("index.html", title=APP_NAME, terms=POSSIBLE_TERMS, subs=SUB_CODES_TO_SUB_SET,
                                request_message=status)
+
+
+@_app.route("/delete", methods=["POST"])
+def unsubscribe_user():
+    try:
+        from_number = request.form['From']
+        body = str(request.form['Body']).strip().lower()
+        body_length = len(body)
+
+        # 'cancel all' --> delete all requests that exists with the this phone number
+        if body_length == 10:
+            RequestRepository(mysql.connection).delete_request(from_number)
+            # notify That All Reqs Are Deleted
+        # 'cancel (5-digit-code)' --> delete the request that exists with the this phone number and 5-digit-class-num
+        elif body_length == 11:
+            cls_5_digit = body.split(" ")[1]
+            if len(cls_5_digit) == 5 and cls_5_digit.isdigit():
+                RequestRepository(mysql.connection).delete_request(from_number, int(cls_5_digit))
+                # notify That specific req is Deleted
+    except:
+        pass
 
 
 if __name__ == "__main__":
