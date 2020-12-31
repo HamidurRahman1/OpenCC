afbbfafb8be4d7735fbae4b8fcb4cdbc8f414b92
Hamidur Rahman <Hamidur0405@gmail.com>
2020-12-31 16:12:56 -0500

    dev ex removed

--- edu/lagcc/opencc/exceptions/exceptions.py
+++ edu/lagcc/opencc/exceptions/exceptions.py
@@ -36,16 +36,3 @@ class NotFoundException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
-
-
-class NotifyDeveloperException(Exception):
-    """
-    Exception raised when cause is completely unknown and/or database is down, and/or application is down.
-    Attributes:
-        error_type --> indicating the existing phone number
-        partial_message --> a message to get hint of the error
-    """
-
-    def __init__(self, error_type, partial_message):
-        self.message = "Dev: Exception of type '{0}' with arguments:\n{1!r}".format(error_type, partial_message)
-        super().__init__(self.message)
