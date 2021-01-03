90bcd0f391f3efba95429ee4c72656a7da68d3d2
Hamidur Rahman <Hamidur0405@gmail.com>
2021-01-03 17:37:25 -0500

    using localhost db

--- edu/lagcc/opencc/config/config.py
+++ edu/lagcc/opencc/config/config.py
@@ -4,7 +4,7 @@ from os import environ
 TESTING = False
 DEBUG = True
 
-MYSQL_HOST = environ.get("MYSQL_HOST")
-MYSQL_USER = environ.get("MYSQL_USER")
-MYSQL_PASSWORD = environ.get("MYSQL_PASSWORD")
-MYSQL_DB = environ.get("MYSQL_DB")
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "mysql?"
+MYSQL_DB = "opencc_db"
