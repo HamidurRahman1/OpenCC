cbb1b925c0fe394a2f18be1363d2aa6016a234b4
Paul Alarcon <pa.f.e@hotmail.com>
2021-02-08 19:37:09 -0500

    fixed request

---
+++ static/script/request.js
@@ -0,0 +1,19 @@
+const add_request_form = document.querySelector('#add-request');
+let formData;
+add_request_form.onsubmit = evt => {
+  evt.preventDefault();
+  formData = new FormData(add_request_form);
+  requestInfo = {
+    "phone-number" : formData.get('phone-number'),
+    "term" : formData.get('term'),
+    "subject" : formData.get('subject'),
+    "class-num-5" : formData.get('class-num-5'),
+  }
+  fetch('/api/request', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestInfo)
+  }).then(data => console.log(data));
+}
